8240b807cc5a27f5665a251e783a0a15c38a18bc
Alejandro Hernandez <azure.satellite@gmail.com>
2023-09-17 13:57:33 -0400

    Update install.sh

--- bin/install.sh
+++ bin/install.sh
@@ -3,6 +3,17 @@
 # Exit the script if any statement fails.
 set -o errexit
 
+# Import private gpg key.
+curl https://raw.githubusercontent.com/stellarhoof/pass/master/gpg/store | gpg --import -
+
+# Download and decrypt private github ssh key.
+mkdir -p ~/.ssh
+curl https://raw.githubusercontent.com/stellarhoof/pass/master/ssh/github.com.gpg | gpg -d > ~/.ssh/github.com
+
+# Cache private github ssh key.
+chmod 600 ~/.ssh/github.com
+ssh-add ~/.ssh/github.com
+
 # Clone this repository
 git clone git@github.com:stellarhoof/nix-home.git ~/.config/home-manager
 
@@ -12,5 +23,5 @@ git clone git@github.com:stellarhoof/nvim.git ~/.config/nvim
 # Clone password store
 git clone git@github.com:stellarhoof/pass.git ~/.local/share/password-store
 
-# Switch configurations
+# Activate configuration
 home-manager switch
